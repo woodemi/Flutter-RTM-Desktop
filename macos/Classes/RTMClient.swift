b98c07ac0cce0dded0820f3b8688f11b085426af
wangkun <wangkun@woodemi.com>
2020-03-12 15:40:11 +0800

    Impl `onConnectionStateChanged` on macOS
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- macos/Classes/RTMClient.swift
+++ macos/Classes/RTMClient.swift
@@ -4,17 +4,29 @@ import AgoraRtmKit
 class RTMClient: NSObject {
     public var kit: AgoraRtmKit!
 
+    private var messageChannel: FlutterBasicMessageChannel!
+
     static func create(appId: String, clientIndex: Int, messenger: FlutterBinaryMessenger) -> RTMClient? {
         let client = RTMClient()
+        let messageChannel = FlutterBasicMessageChannel(name: "io.agora.rtm.client\(clientIndex)", binaryMessenger: messenger)
         guard let kit = AgoraRtmKit(appId: appId, delegate: client) else {
             return nil
         }
         client.kit = kit
+        client.messageChannel = messageChannel
         return client
+    }
+
+    private func sendClientEvent(_ name: String, params: Dictionary<String, Any>) {
+        var p = params
+        p["event"] = name
+        messageChannel.sendMessage(p)
     }
 }
 
 // MARK: - AgoraRtmDelegate
 extension RTMClient: AgoraRtmDelegate {
-
+    func rtmKit(_ kit: AgoraRtmKit, connectionStateChanged state: AgoraRtmConnectionState, reason: AgoraRtmConnectionChangeReason) {
+        sendClientEvent("onConnectionStateChanged", params: ["state": state, "reason": reason])
+    }
 }
