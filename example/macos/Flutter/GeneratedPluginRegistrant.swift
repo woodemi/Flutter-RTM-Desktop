85c9b15ead9f817dd7bda892f9279d50d5c733c6
wangkun <wangkun@woodemi.com>
2020-03-12 12:37:59 +0800

    Initial commit
    Signed-off-by: wangkun <wangkun@woodemi.com>

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import agora_rtm
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AgoraRtmPlugin.register(with: registry.registrar(forPlugin: "AgoraRtmPlugin"))
+}
